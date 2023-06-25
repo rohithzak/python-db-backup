093d9b8f946489ea5cea9b4bb18e88a9f5d2cc22
rohithzak <134053766+rohithzak@users.noreply.github.com>
2023-06-26 02:19:43 +0530

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-action=cp
-
+action=<Enter your action> # cp, scp, s3
 
 #common
-sed -i "s/cron = 0/cron = 1/g" backup.testnew.py
-sed -i "s/action = 'null'/action = '$action'/g" backup.testnew.py
-sed -i "s/database_type = 0/database_type = 3/g" backup.testnew.py
+sed -i "s/cron = 0/cron = 1/g" <python script>
+sed -i "s/action = 'null'/action = '$action'/g" <python sscript>
+sed -i "s/database_type = 0/database_type = 3/g" <python script>   # 1 -postgres, 2-Mysql, 3-Mongo
 
 #For mysql
-python3  python_script.py username passsword dbname dumpfile 
+python3  <python script> username passsword dbname dumpfile 
 
 #For Mongo
-python3  python_script.py dbname username password dumpfile
+python3  <python script> dbname username password dumpfile
 
 #For Postgres
-python3  python_script.py dbname dumpfile
+python3 <python script> dbname dumpfile
 
 
 #common
-sed -i "s/database_type = 3/database_type = 0/g" backup.testnew.py
-sed -i "s/action = '$action'/action = 'null'/g" backup.testnew.py
-sed -i "s/cron = 1/cron = 0/g" backup.testnew.py
+sed -i "s/database_type = 3/database_type = 0/g" <python script>    #input same value in which you gave above for database type
+sed -i "s/action = '$action'/action = 'null'/g" <python script>
+sed -i "s/cron = 1/cron = 0/g" <python script>
